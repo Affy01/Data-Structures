972d59619ded0e3a3229b18248a78cee89320514
Affy01 <afzals@bu.edu>
2015-12-28 18:07:34 -0500

    Create Edge.java

---
+++ Dijkstra/Edge.java
@@ -0,0 +1,8 @@
+package shortestpath;
+
+public class Edge {
+	Node 	start;	/* initial vertex of this edge */
+	Node 	end;	/* terminal vertex of this edge */
+	int	len;		/* length */
+	String	name;
+}
